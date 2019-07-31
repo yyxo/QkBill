ece96b6112da3b06a37a4e2dac51b3a88c6a6e69
macarons <w_wubo@163.com>
2019-07-31 16:47:38 +0800

    修改以太坊节点
    修改以太坊节点

--- js/app.js
+++ js/app.js
@@ -72,6 +72,17 @@ function getNodeHost()
 	return node_host;
 }
 
+function getEthNodeHost()
+{
+	var node_host = localStorage.getItem("eth_node_host");
+	if(!node_host)
+	{
+		var node_host = "http://eth-node.hz.quarkblockchain.cn:8546";
+	}
+	
+	return node_host;
+}
+
 function setAdaptionHeight(className,proportion)
 {
 	var Bgwidth = h("."+className).width();
@@ -748,7 +759,9 @@ function install_wgt(filename)
  */
 function updateEthAmount(address)
 {
-	var provider = new ethers.providers.EtherscanProvider("homestead","TJDWN2WQI6WF4ERNW4FDXJGB9DSQ881P7J");
+	console.log(getEthNodeHost())
+	var provider = new ethers.providers.JsonRpcProvider({"url":getEthNodeHost()});
+
 	provider.getBalance(address).then((balance) => {
 		let etherString = ethers.utils.formatEther(balance);
 		assetsList = plus.storage.getItem(address + "-assets");
@@ -788,7 +801,7 @@ function updateEthTokenAmount(address,assets_name)
 	// abi json 对象
 	const abi = [ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "remaining", "type": "uint256" } ], "payable": false, "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_from", "type": "address" }, { "indexed": true, "name": "_to", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "_owner", "type": "address" }, { "indexed": true, "name": "_spender", "type": "address" }, { "indexed": false, "name": "_value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "inputs": [ { "name": "_initialAmount", "type": "uint256" }, { "name": "_tokenName", "type": "string" }, { "name": "_decimalUnits", "type": "uint8" }, { "name": "_tokenSymbol", "type": "string" } ], "payable": false, "type": "constructor" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }, { "name": "_extraData", "type": "bytes" } ], "name": "approveAndCall", "outputs": [ { "name": "success", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "version", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "type": "function" } ];
 	// 创建一个连接主网络的 provider
-	var provider = new ethers.providers.EtherscanProvider("homestead","TJDWN2WQI6WF4ERNW4FDXJGB9DSQ881P7J");
+	var provider = new ethers.providers.JsonRpcProvider({"url":getEthNodeHost()});
 	// 创建智能合约
 	const contract = new ethers.Contract(token, abi, provider);
 
